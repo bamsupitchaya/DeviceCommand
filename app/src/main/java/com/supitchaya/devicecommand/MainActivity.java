edb7aff73427de0bfa9b775a39c1b1eac0eb7741
supitchaya <bamsupitchay@gmail.com>
2018-10-21 11:22:11 +0700

    onClick

--- app/src/main/java/com/supitchaya/devicecommand/MainActivity.java
+++ app/src/main/java/com/supitchaya/devicecommand/MainActivity.java
@@ -2,6 +2,8 @@ package com.supitchaya.devicecommand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +16,31 @@ public class MainActivity extends AppCompatActivity {
 
     private String string = "Plase Speak Command";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ShowCommand(string);
+
+
+
+      //  Mic Controller
+        micController();
     }// main method
+
+    private void micController() {
+        ImageView imageView = findViewById(R.id.imvMicrophone);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ShowCommand(string);
+
+            } // on click
+        });
+    }
 
     private void ShowCommand(String teststring){
         Toast.makeText(MainActivity.this, teststring, Toast.LENGTH_SHORT).show();
